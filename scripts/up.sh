56de1e0d8339a66c00f521ba94fec509a70baa1f
Marcin Czyż <m.czyz@gmi.pl>
2020-08-14 17:21:17 +0200

    Copy env files in up.sh script

--- scripts/up.sh
+++ scripts/up.sh
@@ -4,12 +4,15 @@ repo=$(pwd)
 docker-compose up -d
 
 cd "$repo"/services/users
+cp .env.dist .env
 docker-compose up -d
 
 cd "$repo"/services/api-gateway
+cp .env.dist .env
 docker-compose up -d
 
 cd "$repo"/services/feed
+cp .env.dist .env
 docker-compose up -d
 
 echo "Containers are up, waiting 30s for databases to bootstrap"
